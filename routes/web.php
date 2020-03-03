98a80be5f37612312a5287fe955cb4a16f4b8f0c
yuze.wang <yuze89@gmail.com>
2020-03-03 11:18:23 +0800

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
 Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
