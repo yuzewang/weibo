9124d733140ae357749af85498d3d84cd5a5338b
yuze.wang <yuze89@gmail.com>
2020-03-08 10:47:51 +0800

    用户微博列表

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,8 +13,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
+        Model::unguard();
 
         $this->call(UsersTableSeeder::class);
+        $this->call(StatusesTableSeeder::class);
+
+        Model::reguard();
 
 
     }
